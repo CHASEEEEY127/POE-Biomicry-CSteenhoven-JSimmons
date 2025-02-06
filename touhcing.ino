425f811c8a632b75f3e5b42cfd202c4307d03c32
CHASEEEEY127 <131933003+CHASEEEEY127@users.noreply.github.com>
2025-02-05 21:38:17 -0500

    Initial commit

---
+++ touhcing.ino
@@ -0,0 +1,33 @@
+
+int waitTime = 2;
+int ticksPerSecond =100;
+int waitTicks=waitTime*ticksPerSecond;
+int waitTimer=0;
+
+int threshHold= 30;
+int happyPin = 23;
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(happyPin,OUTPUT);
+  pinMode(4,INPUT);
+}
+
+void loop() {
+
+  if(touchRead(4)>threshHold){
+    waitTimer=0;
+  }
+  else{
+      waitTimer+=1;
+    }
+
+  if(waitTimer>=waitTicks){
+    digitalWrite(happyPin,LOW);
+  }
+
+  else{
+    digitalWrite(happyPin,HIGH);
+  }
+}
